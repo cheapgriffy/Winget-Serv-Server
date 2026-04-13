27d43addf06d18cfb4f2af2a5a544949c6ca58ab
Griffy <timeo.felix.aux@gmail.Com>
2026-04-13 15:39:12 +0200

    feat: Added ratelimit to all routes

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require("cors")
+const rateLimit = require('express-rate-limit')
+
 
 app.use(express.json())
+
 
 const launchParams = require('./src/config/launch.params').configVariables
 const configDB = require('./src/config/db')
 const scriptRoute = require('./src/routes/scripts.route')
 const userRoute = require('./src/routes/user.route')
 const healthRoute = require("./src/routes/health.route")
-
-// Adds headers: Access-Control-Allow-Origin: *
-app.use(cors())
 
 // runned here to prevent circular dependency
 if (launchParams.INIT_DB == true) {
@@ -22,6 +22,20 @@ if (launchParams.INIT_DB == true) {
         process.exit(0);
     });
 }
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,   // second to minute, 15m
+    max: 100,                   // during windowMs
+    message: 'Too many request, rate limit triggered',
+    standardHeaders: true,      // output header limit in header
+    legacyHeaders: false,       // prevent outdate header synhtax
+})
+
+
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors())
+// set on all routes
+app.use(limiter)
 
 app.use('/', healthRoute)
 app.use("/user", userRoute)
